0790363dcf14df9897137a841ef3aa4ca1de8824
suinua <maatinosippo@gmail.com>
2018-10-27 15:23:17 +0900

    [update]
    Responseでいちいちステータスをせっていしなくていいように
    bodyの型をdynamicに

--- lib/src/http/model/Response.dart
+++ lib/src/http/model/Response.dart
@@ -1,24 +1,63 @@
 import 'dart:convert';
 
+import 'dart:io';
+
 class Response {
   dynamic body;
   int status;
-  String contentType;
+  ContentType contentType;
 
-  text(){
-    this.contentType = "text/plain";
-  }
-  json(){
-    this.body = jsonEncode(this.body); 
-    this.contentType = "application/json";
+  text() {
+    this.contentType = ContentType.text;
   }
 
-  send(response){
-    response.headers.set("Content-Type", this.contentType);
+  json() {
+    this.body = jsonEncode(this.body);
+    this.contentType = ContentType.json;
+  }
+
+  send(HttpResponse response) {
+    response.headers.contentType = this.contentType;
     response.statusCode = this.status;
     response.write(this.body);
     response.close();
   }
 
-  Response(this.body,this.status):this.contentType = "text/plain";
+  Response(this.body, this.status) : this.contentType = ContentType.text;
+
+  Response.ok(this.body)
+      : this.contentType = ContentType.text,
+        this.status = 200;
+
+  Response.created(this.body)
+      : this.contentType = ContentType.text,
+        this.status = 201;
+
+  Response.movedPermanently(this.body)
+      : this.contentType = ContentType.text,
+        this.status = 301;
+
+  Response.notModified(this.body)
+      : this.contentType = ContentType.text,
+        this.status = 302;
+
+  Response.badRequest(this.body)
+      : this.contentType = ContentType.text,
+        this.status = 400;
+
+  Response.unauthorixed(this.body)
+      : this.contentType = ContentType.text,
+        this.status = 401;
+
+  Response.forbidden(this.body)
+      : this.contentType = ContentType.text,
+        this.status = 403;
+
+  Response.notFound(this.body)
+      : this.contentType = ContentType.text,
+        this.status = 404;
+
+  Response.gone(this.body)
+      : this.contentType = ContentType.text,
+        this.status = 410;
 }
