ae0057a446a74b6de730f10b40800755db68c8f6
suinua <maatinosippo@gmail.com>
2018-10-31 00:45:14 +0900

    fix

--- lib/src/nepuro_base.dart
+++ lib/src/nepuro_base.dart
@@ -12,112 +12,112 @@ class Nepuro {
     print("start");
     print("http://${ip}:${port}");
 
-    HttpServer.bind(ip, port).then((server) {
-      server.listen((HttpRequest request) async {
-        print("[${request.method}] ${request.uri.path}");
+    var server = await HttpServer.bind(ip, port);
 
-        HttpResponse response = request.response;
+    await for (HttpRequest request in server) {
+      print("[${request.method}] ${request.uri.path}");
 
-        //@Routeのつく関数、メタデータをすべて取得
-        final List<Route> routeList = getRouteList();
-        //routeListのなかからpath,methodが一致する関数のみ取得
-        final Route route = await getMatchRoute(request, routeList);
+      HttpResponse response = request.response;
 
-        //routeが空かNullなら　404を返す
-        if (route == null) {
-          Response.notFound("not found").send(response);
-          print("status: 404");
+      //@Routeのつく関数、メタデータをすべて取得
+      final List<Route> routeList = getRouteList();
+      //routeListのなかからpath,methodが一致する関数のみ取得
+      final Route route = await getMatchRoute(request, routeList);
 
-          return 404;
-        }
+      //routeが空かNullなら　404を返す
+      if (route == null) {
+        Response.notFound("not found").send(response);
+        print("status: 404");
 
-        //ライブラリの利用者に返すデータ
-        dynamic body;
-        Map<String, dynamic> returnReqData = {"body": null, "path": null};
+        return 404;
+      }
 
-        //ライブラリの利用者がpathデータを要求していたら
-        if (route.isCallVarPath) {
-          returnReqData["path"] = request.uri.pathSegments.last;
-        }
+      //ライブラリの利用者に返すデータ
+      dynamic body;
+      Map<String, dynamic> returnReqData = {"body": null, "path": null};
 
-        //ライブラリ利用者がbodyに型を設定していなければ
-        if (!route.isCallBody) {
-          route.sendResponse(returnReqData, response);
-          print("status: ${response.statusCode}");
+      //ライブラリの利用者がpathデータを要求していたら
+      if (route.isCallVarPath) {
+        returnReqData["path"] = request.uri.pathSegments.last;
+      }
 
-          return response.statusCode;
-        }
-
-        //ライブラリ利用者がbodyに設定しているクラスがRequestBodyTypeを実装しているか
-        bool isBodyObject = getBodyType(route.method)
-            .superinterfaces
-            .contains(reflectClass(BodyObject));
-
-        //ライブラリ利用者がbodyに設定した型とリクエストの型が一致するかどうか
-        ContentType requestContentType = RequestBody().toContentType(request);
-        Map<Type, ContentType> contentTypeList = {String: ContentType.text};
-        bool isNotCorrectContentType() {
-          if (isBodyObject && requestContentType == ContentType.json) {
-            return false;
-          } else if (contentTypeList[getBodyType(route.method).reflectedType] ==
-              requestContentType) {
-            return false;
-          }
-          return true;
-        }
-
-        //一致していなければ400
-        if (isNotCorrectContentType()) {
-          Response.badRequest("bad request").send(response);
-          print("status: 400");
-          return 400;
-        }
-
-        //ContentTypeがtextならそのまま代入して返す
-        if (requestContentType == ContentType.text) {
-          await RequestBody().parse(request).then((requestBody) {
-            returnReqData["body"] = requestBody;
-          });
-          route.sendResponse(returnReqData, response);
-          print("status: ${response.statusCode}");
-
-          return response.statusCode;
-        }
-
-        //
-        if (!isBodyObject) {
-          Response.badRequest("bad request").send(response);
-
-          print("status: 400");
-          return 400;
-        }
-
-        await RequestBody().parse(request).then((requestBody) {
-          body = requestBody;
-        });
-
-        isBadRequest() {
-          var isContainNull = (Map map) => map.values.toList().contains(null);
-
-          bool isBodyNotCorrect =
-              route.requiredField.isEmpty ? false : !route.isCorrectBody(body);
-
-          return route.requiredField.isEmpty &&
-                  isContainNull(returnReqData["body"].asMap()) ||
-              isBodyNotCorrect;
-        }
-
-        if (isBadRequest()) {
-          Response.badRequest("bad request").send(response);
-          print("status: 400");
-          return 400;
-        }
-
-        returnReqData["body"] = toBodyType(route.method, body);
+      //ライブラリ利用者がbodyに型を設定していなければ
+      if (!route.isCallBody) {
         route.sendResponse(returnReqData, response);
         print("status: ${response.statusCode}");
-        return 200;
+
+        return response.statusCode;
+      }
+
+      //ライブラリ利用者がbodyに設定しているクラスがRequestBodyTypeを実装しているか
+      bool isBodyObject = getBodyType(route.method)
+          .superinterfaces
+          .contains(reflectClass(BodyObject));
+
+      //ライブラリ利用者がbodyに設定した型とリクエストの型が一致するかどうか
+      ContentType requestContentType = RequestBody().toContentType(request);
+      Map<Type, ContentType> contentTypeList = {String: ContentType.text};
+      bool isNotCorrectContentType() {
+        if (isBodyObject && requestContentType == ContentType.json) {
+          return false;
+        } else if (contentTypeList[getBodyType(route.method).reflectedType] ==
+            requestContentType) {
+          return false;
+        }
+        return true;
+      }
+
+      //一致していなければ400
+      if (isNotCorrectContentType()) {
+        Response.badRequest("bad request").send(response);
+        print("status: 400");
+        return 400;
+      }
+
+      //ContentTypeがtextならそのまま代入して返す
+      if (requestContentType == ContentType.text) {
+        await RequestBody().parse(request).then((requestBody) {
+          returnReqData["body"] = requestBody;
+        });
+        route.sendResponse(returnReqData, response);
+        print("status: ${response.statusCode}");
+
+        return response.statusCode;
+      }
+
+      //
+      if (!isBodyObject) {
+        Response.badRequest("bad request").send(response);
+
+        print("status: 400");
+        return 400;
+      }
+
+      await RequestBody().parse(request).then((requestBody) {
+        body = requestBody;
       });
-    });
+
+      isBadRequest() {
+        var isContainNull = (Map map) => map.values.toList().contains(null);
+
+        bool isBodyNotCorrect =
+            route.requiredField.isEmpty ? false : !route.isCorrectBody(body);
+
+        return route.requiredField.isEmpty &&
+                isContainNull(returnReqData["body"].asMap()) ||
+            isBodyNotCorrect;
+      }
+
+      if (isBadRequest()) {
+        Response.badRequest("bad request").send(response);
+        print("status: 400");
+        return 400;
+      }
+
+      returnReqData["body"] = toBodyType(route.method, body);
+      route.sendResponse(returnReqData, response);
+      print("status: ${response.statusCode}");
+      return 200;
+    }
   }
 }
