83b75af40dd913bf46b2c53a20c97a0cb3c3822b
suinua <maatinosippo@gmail.com>
2018-11-07 16:34:30 +0900

    名称変更
    pathVar→pathParameter

--- lib/src/annotation/call.dart
+++ lib/src/annotation/call.dart
@@ -1,10 +1,10 @@
 class Call {
   final String type;
 
-  final pathVarName;
+  final pathParameterName;
   
   final String contentType;
 
-  const Call.path(this.pathVarName):this.type = "path",this.contentType = null;
-  const Call.body({this.contentType}):this.type = "body",this.pathVarName = null;
+  const Call.path(this.pathParameterName):this.type = "path",this.contentType = null;
+  const Call.body({this.contentType}):this.type = "body",this.pathParameterName = null;
 }
--- lib/src/call_back_data/pathParameter.dart
+++ lib/src/call_back_data/pathParameter.dart
@@ -8,25 +8,25 @@ class CallBackPathParameter {
       Map<String, dynamic> pathSegments) {
     Map<String, dynamic> result = new Map();
     
-    for (var pathVarType in pathParameterTypes) {
-      String pathVarName = pathVarType.metadata.first.reflectee.pathVarName;
-      Type pathType = pathVarType.type.reflectedType;
+    for (var pathParameterType in pathParameterTypes) {
+      String parameterName = pathParameterType.metadata.first.reflectee.pathParameterName;
+      Type pathType = pathParameterType.type.reflectedType;
 
-      if (null == pathSegments[pathVarName]) {
-        print("Call.path(\"$pathVarName\") is not exist.");
+      if (null == pathSegments[parameterName]) {
+        print("Call.path(\"$parameterName\") is not exist.");
       } else {
         switch (pathType) {
           case String:
-            result[pathVarName] = pathSegments[pathVarName].toString();
+            result[parameterName] = pathSegments[parameterName].toString();
             break;
           case int:
-            result[pathVarName] = int.parse(pathSegments[pathVarName]);
+            result[parameterName] = int.parse(pathSegments[parameterName]);
             break;
           case dynamic:
-            result[pathVarName] = pathSegments[pathVarName];
+            result[parameterName] = pathSegments[parameterName];
             break;
           default:
-            result[pathVarName] = pathSegments[pathVarName].toString();
+            result[parameterName] = pathSegments[parameterName].toString();
         }
       }
     }
